dc9c2ee5c812509739d17e89b019067109632670
jghil <jayjeonghil94@gmail.com>
2022-06-21 06:10:44 +0000

    completed meme-gallery project

--- main.js
+++ main.js
@@ -1,10 +1,28 @@
-var imageUrls = [];
+var $gallery = document.querySelector('main');
+var imageUrls = [
+  'https://i.kym-cdn.com/entries/icons/original/000/013/564/doge.jpg',
+  'https://i.kym-cdn.com/entries/icons/original/000/027/475/Screen_Shot_2018-10-25_at_11.02.15_AM.png',
+  'https://assets-global.website-files.com/5f3c19f18169b62a0d0bf387/60d33be7eedf8e1f31aabcec_BwENfmI0CU5dZGYlSyo142mpfG08-rYgTS-Qm47uMUXN6JXtmdZvtzVzTooUQdXTWmTD8uzF9N6XQJA2vUIMi53tunFyVtvOBJTNfOjHit2P_JkTmFzFsK7ep6Vb9781XZnRAryH.png'
+];
 var $imageUrlInput = document.querySelector('input');
 var $addImageButton = document.querySelector('button');
 
 $addImageButton.addEventListener('click', function () {
-  var inputValue = $imageUrlInput.value;
-  imageUrls.push($imageUrlInput.value);
-  $imageUrlInput.value = '';
-  console.log(inputValue);
+  if ($imageUrlInput.value !== '') {
+    imageUrls.push($imageUrlInput.value);
+    $imageUrlInput.value = '';
+    updateGallery();
+  }
 });
+
+function updateGallery() {
+  $gallery.innerHTML = '';
+  for (var i = 0; i < imageUrls.length; i++) {
+    var $imageElement = document.createElement('img');
+    $imageElement.className = 'gallery-image';
+    $imageElement.src = imageUrls[i];
+    $gallery.appendChild($imageElement);
+  }
+}
+
+updateGallery();
